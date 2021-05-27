8b3938060f543f032a54a751f1fbcb229562198c
brilyanilhamsadewo <brilyan.ilham9i.36@gmail.com>
2021-05-27 21:55:43 +0700

    Armada

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,12 @@ $router->group(['middleware' => 'auth'], function() use($router) {
     $router->get('/categories/{id}', 'CategoryController@edit');
     $router->put('/categories/{id}', 'CategoryController@update');
     $router->delete('/categories/{id}', 'CategoryController@destroy');
+
+    $router->get('fleets', 'FleetsController@index');
+    $router->post('fleets', 'FleetsController@store');
+    $router->get('fleets/{id}', 'FleetsController@edit');
+    $router->put('fleets/{id}', 'FleetsController@update');
+    $router->delete('fleets/{id}', 'FleetsController@destroy');
 });
 
 
